71f7574c608cb93554c3cdcf7d079876b2fea7f9
Ringo Alcock <ringo.alcock@gmail.com>
2021-01-20 21:13:36 -0800

    add video create method

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,7 +21,27 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+    if Video.find_by(external_id: params[:external_id])
+      render status: :bad_request, json: { errors: "this film is already in your library" }
+      return
+    end
+
+    video = Video.new(video_params)
+    if video.save
+      render status: :ok, json: {}
+      return
+    else
+      render status: :bad_request, json: { errors: video.errors.messages }
+      return
+    end
+  end
+
   private
+
+  def video_params
+    params.require(:video).permit(:title, :overview, :release_date, :inventory, :image_url, :external_id)
+  end
 
   def require_video
     @video = Video.find_by(title: params[:title])
